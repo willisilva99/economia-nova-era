f3bfe391c6388a5fd6ab559bee58edf9644697f3
willisilva99 <126760543+willisilva99@users.noreply.github.com>
2024-10-20 09:04:55 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord import Embed
 import os
 import vip  # Importa as funções de vip.py
 
@@ -10,11 +11,6 @@ intents.members = True
 
 bot = commands.Bot(command_prefix="!!", intents=intents)
 
-# Comando para iniciar o processo de compra de VIP
-@bot.command(name="comprarvip")
-async def comprar_vip(ctx):
-    await vip.comprar_vip(ctx, bot)
-
 # Rodar o bot
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("TOKEN")  # Certifique-se de definir a variável de ambiente TOKEN
 bot.run(TOKEN)
